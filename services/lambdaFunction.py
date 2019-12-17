8cc3480df32d0bc4eae759ff72713225a1531425
Niroshan Kumarasinghe <niroshanku@jkintranet.com>
2019-12-17 10:58:43 +0530

    changes to the function

--- services/lambdaFunction.py
+++ services/lambdaFunction.py
@@ -1,1 +1,7 @@
-print("This line will be printed.")
+import boto3
+import json
+
+ec2 = boto3.client('ec2')
+def lambda_handler(event, context):
+    response = ec2.describe_availability_zones()
+    return {"statusCode": 200, "body": json.dumps(response)}
