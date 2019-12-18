51f0f0ef50edceeda2e062973ed936de35b7837d
Niroshan Kumarasinghe <niroshanku@jkintranet.com>
2019-12-18 22:52:04 +0530

    create new services

---
+++ services/car-model-search.py
@@ -0,0 +1,8 @@
+from __future__ import print_function
+import boto3
+import json
+import decimal
+from 
+
+# Helper class to convert a DynamoDB item to JSON.
+class Decimal
--- services/lambdaFunction.py
+++ services/lambdaFunction.py
@@ -1,5 +1,18 @@
+from __future__ import print_function # Python 2/3 compatibility
 import json
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
 
-def lambda_handler(event, context):
-    response = "This is for test"
-    return {"statusCode": 200, "body": json.dumps(response)}
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('Music')
+
+def handler(event, context):    
+    print("Joe's music")
+    print(table.creation_date_time)
+
+response = table.query(
+    KeyConditionExpression=Key('Artist').eq('Joe')
+)
+
+for i in response['Items']:
+    print(i['Artist'], ":", i['Artist'])
